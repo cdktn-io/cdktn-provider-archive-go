5ac817f0086f5aa6df32dceb9e3ccb89c558cb4a
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2025-06-04 18:45:19 +0000

    chore(release): v11.0.0

--- archive/file/File.go
+++ archive/file/File.go
@@ -5,10 +5,10 @@ package file
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-archive-go/archive/v10/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-archive-go/archive/v11/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-archive-go/archive/v10/file/internal"
+	"github.com/cdktf/cdktf-provider-archive-go/archive/v11/file/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
