7e14cbbf0b92cf7c9c9f041ef248df0ae2ff643c
team-cdk-terrain <github-team-cdk-terrain@cdktn.io>
2026-05-07 17:23:43 +0000

    chore(release): v13.0.1

--- archive/dataarchivefile/DataArchiveFile.go
+++ archive/dataarchivefile/DataArchiveFile.go
@@ -5,10 +5,10 @@ package dataarchivefile
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktn-io/cdktn-provider-archive-go/archive/v12/jsii"
+	_init_ "github.com/cdktn-io/cdktn-provider-archive-go/archive/v13/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktn-io/cdktn-provider-archive-go/archive/v12/dataarchivefile/internal"
+	"github.com/cdktn-io/cdktn-provider-archive-go/archive/v13/dataarchivefile/internal"
 	"github.com/open-constructs/cdk-terrain-go/cdktn"
 )
 
@@ -139,6 +139,15 @@ type DataArchiveFile interface {
 	// Adds this resource to the terraform JSON output.
 	// Experimental.
 	ToTerraform() interface{}
+	// Applies one or more mixins to this construct.
+	//
+	// Mixins are applied in order. The list of constructs is captured at the
+	// start of the call, so constructs added by a mixin will not be visited.
+	// Use multiple `with()` calls if subsequent mixins should apply to added
+	// constructs.
+	//
+	// Returns: This construct for chaining.
+	With(mixins ...constructs.IMixin) constructs.IConstruct
 }
 
 // The jsii proxy struct for DataArchiveFile
@@ -1181,6 +1190,24 @@ func (d *jsiiProxy_DataArchiveFile) ToTerraform() interface{} {
 		d,
 		"toTerraform",
 		nil, // no parameters
+		&returns,
+	)
+
+	return returns
+}
+
+func (d *jsiiProxy_DataArchiveFile) With(mixins ...constructs.IMixin) constructs.IConstruct {
+	args := []interface{}{}
+	for _, a := range mixins {
+		args = append(args, a)
+	}
+
+	var returns constructs.IConstruct
+
+	_jsii_.Invoke(
+		d,
+		"with",
+		args,
 		&returns,
 	)
 
